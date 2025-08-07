4711224252da9504d5768777f3e3bb5cabf807e4
khantzeyar <khla0002@student.monash.edu>
2025-08-08 05:04:41 +0800

    Implemented the frontend for the Trends page

--- src/app/trends/page.tsx
+++ src/app/trends/page.tsx
@@ -1,8 +1,57 @@
+"use client";
+import 'chart.js/auto';
+import dynamic from 'next/dynamic';
 import React from 'react'
+
+const Bar = dynamic(() => import('react-chartjs-2').then((mod) => mod.Bar), {
+  ssr: false,
+});
+
+const labels = ['Ingredient 5', 'Ingredient 4', 'Ingredient 3', 'Ingredient 2', 'Ingredient 1'];
+const frequencies = [40, 50, 60, 70, 80];
+const ingredientColor = [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+];
+const backgroundColor = ingredientColor.map(rgb =>
+  rgb.replace('rgb', 'rgba').replace(')', ', 0.2)')
+);
+
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'Banned Frequency',
+    data: frequencies,
+    backgroundColor: backgroundColor,
+    borderColor: ingredientColor,
+    borderWidth: 1
+  }]
+};
 
 const TrendsPage = () => {
   return (
-    <div>Trends</div>
+    <div className='w-full flex flex-col items-center justify-center mt-38'>
+      <h1 className='font-bold text-3xl mb-8'>Frequency of the Most Banned Substances</h1>
+      <div className="flex flex-row items-center gap-12">
+        <div className="w-[900px] h-[400px]">
+          <Bar data={data} options={{ responsive: true, maintainAspectRatio: false }} />
+        </div>
+        <ul className="list-disc ml-6 text-lg">
+          {[...labels].reverse().map((ingredient, idx) => (
+            <li
+              key={ingredient}
+              style={{ ['--bullet-color' as string]: ingredientColor[idx] }}
+              className="text-black marker:[color:var(--bullet-color)]"
+            >
+              {ingredient} (<span>{[...frequencies].reverse()[idx]}</span>)
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 }
 
