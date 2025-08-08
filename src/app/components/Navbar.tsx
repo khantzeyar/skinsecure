daf8c09b4c021fb536123ee2717ce1067a1f8fe9
khantzeyar <khla0002@student.monash.edu>
2025-08-09 03:46:44 +0800

    Added the search field to the navbar.

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,6 +3,7 @@
  * - The logo will be displayed on the left
  * - The navigation links ("Products" and "Trends") will be on the left.
  * - The current page will have its link highlighted with a black background and white text.
+ * - TO BE IMPLEMENTED - Search functionality
 */
 
 "use client";
@@ -10,6 +11,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
+import Search from '@mui/icons-material/Search';
 import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
@@ -21,6 +23,19 @@ const Navbar = () => {
         {/* Logo */}
         <Image src="/skinsecure.svg" alt="Logo" 
         width={430} height={147} style={{ height: '100px', width: 'auto'}}/>
+        {/* Search Field - Only shown on the product page*/}
+        {pathname === '/' && (
+        <div className="flex-1 flex justify-center">
+          <div className="relative w-1/2">
+            <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
+            <input 
+              type="text" 
+              placeholder="Search by name or notification number" 
+              className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg focus:outline-none" 
+            />
+          </div>
+        </div>
+      )}
         {/* Navigation Links */}
         <ul className='flex text-lg mr-10'> 
             <li>
