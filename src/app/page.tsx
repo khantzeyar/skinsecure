6276073068e2958f51b7125a17a0db55740d8e6e
khantzeyar <khla0002@student.monash.edu>
2025-08-08 23:34:14 +0800

    Created the design of the approved product card.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import ApprovedProduct from './components/ApprovedProduct';
 
 export default function Home() {
   return (
-    <div>Products</div>
+    <div className='w-full flex flex-col items-center justify-center mt-38'>
+      <ApprovedProduct></ApprovedProduct>
+    </div>
   );
 }
