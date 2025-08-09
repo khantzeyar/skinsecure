0236d89ae1c9201a3e37c45a400c4fa9b83dbe54
khantzeyar <khla0002@student.monash.edu>
2025-08-09 14:31:02 +0800

    Fixed a bug involving the misalignment of the Similar Products button.

--- src/app/components/ApprovedProduct.tsx
+++ src/app/components/ApprovedProduct.tsx
@@ -6,26 +6,30 @@ import React from 'react'
 
 const ApprovedProduct = () => {
   return (
-    <div className='flex flex-col items-center w-70 h-77 
+    <div className='flex flex-col justify-between items-center w-70 h-77 
     border shadow-md rounded border-gray-200 overflow-hidden'> 
-        {/* Header */}
-        <div className='w-full flex flex-col items-center py-1' style={{ backgroundColor: '#2E7D32' }}>
-            <h1 className='font-extrabold text-white'>APPROVED</h1>
-        </div>
-        <h1 className='font-bold py-5'>Product Name</h1>
-        <h1 className='font-normal text-[14px]'>Notif No. - NOT200603276K</h1>
-        <div className="w-full px-7">
-            {/* Divider Line */}
-            <div className="h-px bg-gray-200 mt-5"></div>
-            <h1 className='py-9 text-center text-[14px]'> 
-                The product is safe from any harmful ingredients!
-            </h1>
-            {/* Similar Products Button */}
-            <div className="flex justify-center mt-2">
-                <button className="text-blue-500 cursor-pointer text-[14px]"> 
-                    Similar Products 
-                </button>
+        <div className="w-full">
+            {/* Header */}
+            <div className='w-full flex flex-col items-center py-1' style={{ backgroundColor: '#2E7D32' }}>
+                <h1 className='font-extrabold text-white'>APPROVED</h1>
             </div>
+            <div className="text-center">
+                <h1 className='font-bold py-5'>Product Name</h1>
+                <h1 className='font-normal text-[14px]'>Notif No. - NOT200603276K</h1>
+            </div>
+            <div className="w-full px-7">
+                {/* Divider Line */}
+                <div className="h-px bg-gray-200 mt-5"></div>
+                <h1 className='py-9 text-center text-[14px]'> 
+                    The product is safe from any harmful ingredients!
+                </h1>
+            </div>
+        </div>
+        {/* Similar Products Button */}
+        <div className="flex justify-center mb-6">
+            <button className="text-blue-500 cursor-pointer text-[14px]"> 
+                Similar Products 
+            </button>
         </div>
     </div>
   )
--- src/app/components/CancelledProduct.tsx
+++ src/app/components/CancelledProduct.tsx
@@ -8,35 +8,39 @@ import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 const CancelledProduct = () => {
   return (
-    <div className='flex flex-col items-center w-70 h-77 
+    <div className='flex flex-col justify-between items-center w-70 h-77 
     border shadow-md rounded border-gray-200 overflow-hidden'> 
-        {/* Header */}
-        <div className='w-full flex flex-col items-center py-1' style={{ backgroundColor: '#D50000' }}>
-            <h1 className='font-extrabold text-white'>CANCELLED</h1>
+        <div className="w-full">
+            {/* Header */}
+            <div className='w-full flex flex-col items-center py-1' style={{ backgroundColor: '#D50000' }}>
+                <h1 className='font-extrabold text-white'>CANCELLED</h1>
+            </div>
+            <div className="text-center">
+                <h1 className='font-bold py-5'>Product Name</h1>
+                <h1 className='font-normal text-[14px]'>Notif No. - NOT200603276K</h1>
+            </div>
+            <div className="w-full px-7">
+                {/* Divider Line */}
+                <div className="h-px bg-gray-200 mt-5"></div>
+                <h1 className='font-bold text-center text-[14px] py-2'>High Risk Ingredients</h1>
+                {/* Ingredients List */}
+                <div className="relative flex items-center">
+                    <ul className="list-disc list-inside text-[10px] mb-2 flex-1">
+                        <li>Ingredient 1</li>
+                        <li>Ingredient 2</li>
+                        <li>Ingredient 3</li>
+                        <li>Ingredient 4</li>
+                        <li>Ingredient 5</li>
+                    </ul>
+                    <HelpOutlineIcon className="text-gray-300 ml-2 cursor-pointer" fontSize="large" />
+                </div>
+            </div>
         </div>
-        <h1 className='font-bold py-5'>Product Name</h1>
-        <h1 className='font-normal text-[14px]'>Notif No. - NOT200603276K</h1>
-        <div className="w-full px-7">
-            {/* Divider Line */}
-            <div className="h-px bg-gray-200 mt-5"></div>
-            <h1 className='font-bold text-center text-[14px] py-2'>High Risk Ingredients</h1>
-            {/* Ingredients List */}
-            <div className="relative flex items-center">
-            <ul className="list-disc list-inside text-[10px] mb-2 flex-1">
-                <li>Ingredient 1</li>
-                <li>Ingredient 2</li>
-                <li>Ingredient 3</li>
-                <li>Ingredient 4</li>
-                <li>Ingredient 5</li>
-            </ul>
-            <HelpOutlineIcon className="text-gray-300 ml-2 cursor-pointer" fontSize="large" />
-            </div>
-            {/* Similar Products Button */}
-            <div className="flex justify-center">
-                <button className="text-blue-500 cursor-pointer text-[14px]"> 
-                    Similar Products 
-                </button>
-            </div>
+        {/* Similar Products Button */}
+        <div className="flex justify-center mb-6">
+            <button className="text-blue-500 cursor-pointer text-[14px]"> 
+                Similar Products 
+            </button>
         </div>
     </div>
   )
