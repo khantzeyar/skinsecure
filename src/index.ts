1e734754a7d6d5437203396247af3ef5a5570004
khantzeyar <khla0002@student.monash.edu>
2025-08-10 14:50:13 +0800

    Set up neon and drizzle for my repository.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { drizzle } from 'drizzle-orm/neon-http';
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not set');
+}
+const db = drizzle({ connection: process.env.DATABASE_URL });
